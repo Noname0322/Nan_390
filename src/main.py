d6da76fb167a14e5f60e349c0f1499735f7e4ce9
Noname <zhangjianinggzs@gmail.com>
2024-09-26 15:26:49 +0100

    not raise

--- src/main.py
+++ src/main.py
@@ -32,8 +32,9 @@ def main():
                 try:
                     request = TextToImageRequest.model_validate_json(connection.recv_bytes().decode("utf-8"))
                 except EOFError:
-                    print(f"Inference socket exiting")
 
+                    print(f"Inference socket exiting")
+                    # raise(EOFError)
                     return
 
                 image = infer(request, pipeline)
